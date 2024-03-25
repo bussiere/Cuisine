b494f0d0709dfd8c3f9291a79e20e1f6ea97c5c1
bussiere <bussiere@gmail.com>
2024-03-25 13:19:37 +0100

    wip master Updated: 2024-03-25 13:19:37

--- commit.b.sh
+++ commit.b.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+#!/bin/bash
 sh blacked.sh
-sh sedrep.sh
 git config --global user.name "bussiere"
 git config --global user.email "bussiere@gmail.com"
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
@@ -10,4 +10,4 @@ current="`date +'%Y-%m-%d %H:%M:%S'`"
 msg="Updated: $current"
 git commit -m "wip $branch $msg"
 git push origin $branch
-date
+
